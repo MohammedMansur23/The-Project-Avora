48467597c398459fb9978c2efd6d8d8eebced5be
MohammedMansur23 <classicvictory78@gmail.com>
2026-05-03 01:06:37 +0100

    Add homepage with category filters, product grid, and navbar

---
+++ avora/src/lib/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; 
+import { getDatabase } from "firebase/database"; 
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUkCXYtG0NLdtKQHLcvX7MnS10dlyhAn0",
+  authDomain: "avora-app-7a0a4.firebaseapp.com",
+  databaseURL: "https://avora-app-7a0a4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "avora-app-7a0a4",
+  storageBucket: "avora-app-7a0a4.firebasestorage.app",
+  messagingSenderId: "44245761127",
+  appId: "1:44245761127:web:ca27e54701409bc96268c2"
+};
+
+const app = initializeApp(firebaseConfig); 
+
+export const auth = getAuth(app); 
+export const db = getFirestore(app); 
+export const realtimeDb = getDatabase(app); 
+export const storage = getStorage(app); 
+
+export default app;
